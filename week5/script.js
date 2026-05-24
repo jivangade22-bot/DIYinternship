f05b94d021ff61cf4702eb811c3f8a25df902edc
Jivan Gade <jivangade22@gmail.com>
2026-05-24 21:40:04 +0530

    Completed Week 5 Expandable Rows Task

---
+++ week5/script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const content = button.nextElementSibling;
+
+        if(content.style.display === "block"){
+            content.style.display = "none";
+        }
+
+        else{
+            content.style.display = "block";
+        }
+
+    });
+
+});
